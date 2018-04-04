f4c558451b6203dc4eca7405a26ca3de328d0118
Andrei F <soheevich@gmail.com>
2018-04-04 11:43:20 +0300

    Fix bookcovers loading

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,7 +4,7 @@ const Book = (props) => {
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: props.thumbnail }} />
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.thumbnail})` }} />
         <div className="book-shelf-changer">
           <select>
             <option value="none" disabled>
